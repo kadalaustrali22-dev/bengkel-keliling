1baa2111e201afd98489d041d68a2b5e150a2691
maruf_s400 <kadalaustrali22@gmail.com>
2026-09-19 14:45:08 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Bengkel Keliling</title>
+</head>
+<body>
+
+    <h1>Login Bengkel Keliling</h1>
+
+    <form>
+        <label>Username</label><br>
+        <input type="text"><br><br>
+
+        <label>Password</label><br>
+        <input type="password"><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+
+</body>
+</html>
